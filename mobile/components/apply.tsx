45c7280749076b99dd3b3468e9d1661a01662aac
Maluks12 <malukstito@gmail.com>
2025-10-10 12:18:24 +0200

    apply

--- mobile/components/apply.tsx
+++ mobile/components/apply.tsx
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
         paddingBottom:15,
         fontSize: 16,
         fontWeight: "bold",
-        marginBottom:70,
+        marginBottom:50,
         marginTop: -20
     }
 
